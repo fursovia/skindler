eeb3ae262c978fcc1c63f1d969218088670aad59
i.fursov <i.fursov@tinkoff.ru>
2021-10-22 04:18:22 +0300

    bug fix

--- skindler/commands/attack.py
+++ skindler/commands/attack.py
@@ -1,11 +1,11 @@
 from pathlib import Path
 import json
 from tqdm import tqdm
-import transformers
 
 import torch
 import torch.nn.functional
 from transformers import MarianMTModel, MarianTokenizer
+from transformers.trainer_utils import get_last_checkpoint
 from typer import Typer
 
 from skindler import MODEL_NAME, MAX_LENGTH
@@ -18,8 +18,14 @@ MODELS_FOLDER = (Path(__file__).parent / ".." / "..").resolve() / "models"
 app = Typer()
 
 
-def attack(text: str, autoencoder: MarianAutoEncoder, bleuer: Bleuer, epsilon: float = 0.25):
-    inputs = bleuer.tokenizer(
+def attack(
+        text: str, tokenizer: MarianTokenizer, autoencoder: MarianAutoEncoder, bleuer: Bleuer, epsilon: float = 0.25
+):
+
+    for params in bleuer.parameters():
+        params.grad = None
+
+    inputs = tokenizer(
         text,
         max_length=MAX_LENGTH,
         return_tensors='pt',
@@ -27,11 +33,10 @@ def attack(text: str, autoencoder: MarianAutoEncoder, bleuer: Bleuer, epsilon: f
         truncation=True
     )
 
-    with torch.no_grad():
-        embeddings = bleuer.encoder(**inputs).last_hidden_state
+    embeddings = autoencoder.get_embeddings(**inputs)
     embeddings.requires_grad = True
 
-    bleu = bleuer.forward_on_embeddings(embeddings, inputs["attention_mask"].unsqueeze(-1))
+    bleu = bleuer.get_logits(embeddings)
     loss = torch.nn.functional.l1_loss(bleu, torch.tensor(1.0))
     loss.backward()
 
@@ -39,18 +44,32 @@ def attack(text: str, autoencoder: MarianAutoEncoder, bleuer: Bleuer, epsilon: f
     perturbed_embeddings = embeddings + epsilon * embeddings_grad.sign()
 
     logits = autoencoder.forward_on_embeddings(perturbed_embeddings)
-    decoded = autoencoder.decode_logits(logits)[0]
+    ids = logits.argmax(dim=-1)
+    decoded = tokenizer.decode(ids[0], skip_special_tokens=True, clean_up_tokenization_spaces=True)
     return decoded
 
 
 @app.command()
-def main(dataset_path: Path, save_to: Path, epsilon: float = 0.25):
+def main(
+        dataset_path: Path,
+        ae_dir: Path = Path('experiments/ae'),
+        bl_dir: Path = Path('experiments/bleuer'),
+        save_to: Path = Path('results.json'),
+        epsilon: float = 0.25,
+):
     device = torch.device("cuda")
 
     tokenizer = MarianTokenizer.from_pretrained(MODEL_NAME)
     model = MarianMTModel.from_pretrained(MODEL_NAME).eval().to(device)
-    autoencoder = MarianAutoEncoder.load_from_checkpoint(str(MODELS_FOLDER / "encoder.ckpt"), map_location=device)
-    bleuer = Bleuer.load_from_checkpoint(str(MODELS_FOLDER / "bleuer.ckpt"), map_location=device)
+    ae_path = str(Path(get_last_checkpoint(str(ae_dir))) / 'pytorch_model.bin')
+    bl_path = str(Path(get_last_checkpoint(str(bl_dir))) / 'pytorch_model.bin')
+    autoencoder = MarianAutoEncoder(MODEL_NAME)
+    autoencoder.load_state_dict(torch.load(ae_path))
+    autoencoder.to(device)
+
+    bleuer = Bleuer(MODEL_NAME)
+    bleuer.load_state_dict(torch.load(bl_path))
+    bleuer.to(device)
 
     data = []
     with dataset_path.open() as f:
@@ -64,7 +83,7 @@ def main(dataset_path: Path, save_to: Path, epsilon: float = 0.25):
             ru_trans = example['ru_trans']
             # bleu = example['bleu']
 
-            en_attacked = attack(en, autoencoder=autoencoder, bleuer=bleuer, epsilon=epsilon)
+            en_attacked = attack(en, tokenizer=tokenizer, autoencoder=autoencoder, bleuer=bleuer, epsilon=epsilon)
 
             batch_text_inputs = tokenizer(
                 en_attacked,
@@ -76,7 +95,9 @@ def main(dataset_path: Path, save_to: Path, epsilon: float = 0.25):
             batch_text_inputs.to(device)
 
             output = model.generate(**batch_text_inputs)
-            translations = tokenizer.batch_decode(output, skip_special_tokens=True, clean_up_tokenization_spaces=True)[0]
+            translations = tokenizer.batch_decode(
+                output, skip_special_tokens=True, clean_up_tokenization_spaces=True
+            )[0]
 
             f.write(
                 json.dumps(

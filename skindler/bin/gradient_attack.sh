d100f9d78fa15b8e3f2eb538dddba71b35d8507c
burnyshev <pavel.burnysheff@yandex.ru>
2021-10-31 23:37:15 +0000

    bash script

--- skindler/bin/gradient_attack.sh
+++ skindler/bin/gradient_attack.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-EXPERIMENTFOLDER='../experiment'
+EXPERIMENTFOLDER='../grad_experiment'
 
-for i in {61..90}; do 
+for i in {75..76}; do 
     echo $i; 
     mkdir -p ${EXPERIMENTFOLDER}/threshold_0.${i}
-    python main_attack.py --threshold 0.${i} --experiment_folder ${EXPERIMENTFOLDER}/threshold_0.${i}
+    python skindler/commands/gradient_attack.py --experiment-folder ${EXPERIMENTFOLDER}/threshold_0.${i} --threshold 0.${i}
+    python skindler/commands/get_metrics.py --attack-path ${EXPERIMENTFOLDER}/threshold_0.${i}/attack_output
+    
 done

5dd5ece184a0826b75a6560bed9d026d87c5a925
burnyshev <pavel.burnysheff@yandex.ru>
2021-11-12 02:07:21 +0000

    fix

--- skindler/attackers/__init__.py
+++ skindler/attackers/__init__.py
@@ -1,3 +1,4 @@
 from .attacker import AttackerInput, AttackerOutput, Attacker
 from .gradient_guided_strategy import GradientGuidedAttack
 from .mbart_updated_input import MbartGradientGuidedUpdateInput
+from .mbart_updated_latent import MbartUpdateLatent
--- skindler/bin/gradient_attack.sh
+++ skindler/bin/gradient_attack.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-EXPERIMENTFOLDER='../grad_experiment'
 
 for i in {60..90}; do 
     echo $i; 
-    python skindler/commands/attack.py \
-        skindler/configs/attacks/mbart_gradient_update_input_attack.jsonnet \
-        --out-dir ../skindler_data/attack_output/mbart_attack/mbart_attack_${i}_threshlod \
-        --samples 200 \
-        --threshold ${i}
+#     python skindler/commands/attack.py \
+#         skindler/configs/attacks/mbart_gradient_update_input_attack.jsonnet \
+#         --out-dir ../skindler_data/attack_output/mbart_attack/mbart_attack_${i}_threshlod \
+#         --samples 200 \
+#         --threshold ${i}
+    python skindler/commands/validate.py ../skindler_data/attack_output/mbart_attack/mbart_attack_${i}_threshlod/data.json
     
 done

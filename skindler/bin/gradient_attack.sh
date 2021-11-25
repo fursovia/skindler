b914f009b5157a524979922d97a324c5b7241d1c
burnyshev <pavel.burnysheff@yandex.ru>
2021-11-25 21:20:52 +0000

    fix

--- skindler/bin/gradient_attack.sh
+++ skindler/bin/gradient_attack.sh
@@ -2,11 +2,11 @@
 
 for i in {60..90}; do 
     echo $i; 
-#     python skindler/commands/attack.py \
-#         skindler/configs/attacks/mbart_gradient_update_input_attack.jsonnet \
-#         --out-dir ../skindler_data/attack_output/mbart_attack/mbart_attack_${i}_threshlod \
-#         --samples 200 \
-#         --threshold ${i}
-    python skindler/commands/validate.py ../skindler_data/attack_output/mbart_attack/mbart_attack_${i}_threshlod/data.json
+    python skindler/commands/attack.py \
+        skindler/configs/attacks/gradient_attack.jsonnet \
+        --out-dir ../skindler_data/attack_output/gradient_attack/gradient_attack_${i}_threshlod \
+        --samples 200 \
+        --threshold ${i}
+    python skindler/commands/validate.py ../skindler_data/attack_output/gradient_attack/gradient_attack_${i}_threshlod/data.json
     
 done

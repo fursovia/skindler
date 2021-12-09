b19b335676940b96c954dc1858ed5f112105175c
burnyshev <pavel.burnysheff@yandex.ru>
2021-12-09 18:39:15 +0000

    fix for experiments

--- skindler/bin/gradient_attack.sh
+++ skindler/bin/gradient_attack.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for i in {1..50}; do 
+for i in {1..10}; do 
     echo $i; 
     python skindler/commands/attack.py \
-        skindler/configs/attacks/prefix_gradient_attack.jsonnet \
-        --out-dir ../skindler_data/attack_output/prefix_attack/prefix_attack_${i}_iteration \
-        --samples 50 \
+        skindler/configs/attacks/ner_attack.jsonnet \
+        --out-dir ../skindler_data/attack_output/ner_attack/ner_attack_${i}_iteration \
+        --samples 250 \
         --max-iteration ${i}
-    CUDA_VISIBLE_DEVICES=3 python skindler/commands/validate.py ../skindler_data/attack_output/prefix_attack/prefix_attack_${i}_iteration/data.json
+    CUDA_VISIBLE_DEVICES=3 python skindler/commands/validate.py ../skindler_data/attack_output/ner_attack/ner_attack_${i}_iteration/data.json
     
 done

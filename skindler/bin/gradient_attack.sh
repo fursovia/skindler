5390126c5f8ce9e62b1e40c3f92bf3062869c9e0
burnyshev <pavel.burnysheff@yandex.ru>
2021-12-09 13:50:46 +0000

    small fix

--- skindler/bin/gradient_attack.sh
+++ skindler/bin/gradient_attack.sh
@@ -6,7 +6,7 @@ for i in {1..50}; do
         skindler/configs/attacks/prefix_gradient_attack.jsonnet \
         --out-dir ../skindler_data/attack_output/prefix_attack/prefix_attack_${i}_iteration \
         --samples 50 \
-        --max_iteration ${i}
-    python skindler/commands/validate.py ../skindler_data/attack_output/prefix_attack/prefix_attack_${i}_iteration/data.json
+        --max-iteration ${i}
+    CUDA_VISIBLE_DEVICES=3 python skindler/commands/validate.py ../skindler_data/attack_output/prefix_attack/prefix_attack_${i}_iteration/data.json
     
 done
